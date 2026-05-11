9a872f59a4e18b74bdd62dcb60b602a9be7a7010
amipatel0 <amipatel081103@gmail.com>
2026-05-11 11:03:56 +0530

    add home route

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ connectDB();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
+app.get('/', (req, res) => {
+    res.send('Interest Management Backend Running Successfully');
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/customers', customerRoutes);
 app.use('/api/payments', paymentRoutes);
